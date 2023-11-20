a5c27db479d40673681e9bcf071f8ff0f885c217
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-11-21 00:03:07 +0800

    works

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ constexpr double PI = 3.1415926535897932384626; // M_PI
 constexpr double a = 6378245.0;
 constexpr double ee = 0.00669342162296594323;
 
-inline double transform_lon(double lng, double lat)
+inline double transformlng(double lng, double lat)
 {
     // clang-format off
     double ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * std::sqrt(std::fabs(lng));
@@ -25,7 +25,7 @@ inline double transform_lon(double lng, double lat)
     return ret;
 }
 
-inline double transform_lat(double lon, double lat)
+inline double transformlat(double lng, double lat)
 {
     // clang-format off
     double ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * std::sqrt(std::fabs(lng));
@@ -41,7 +41,7 @@ inline bool out_of_china(double lng, double lat)
     return !(73.66 < lng && lng < 135.05 && 3.86 < lat && lat < 53.55);
 }
 
-inline std::array<double, 2> bd09togcj02(double db_lng, double bd_lat)
+inline std::array<double, 2> bd09togcj02(double bd_lng, double bd_lat)
 {
     double x = bd_lng - 0.0065;
     double y = bd_lat - 0.006;
@@ -71,7 +71,7 @@ inline std::array<double, 2> wgs84togcj02(double lng, double lat,
     double dlat = transformlat(lng - 105.0, lat - 35.0);
     double dlng = transformlng(lng - 105.0, lat - 35.0);
     double radlat = lat / 180.0 * PI;
-    double magic = Math.sin(radlat);
+    double magic = std::sin(radlat);
     magic = 1.0 - ee * magic * magic;
     double sqrtmagic = std::sqrt(magic);
     dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * PI);
@@ -90,11 +90,11 @@ inline std::array<double, 2> gcj02towgs84(double lng, double lat,
     double dlat = transformlat(lng - 105.0, lat - 35.0);
     double dlng = transformlng(lng - 105.0, lat - 35.0);
     double radlat = lat / 180.0 * PI;
-    double magic = Math.sin(radlat);
+    double magic = std::sin(radlat);
     magic = 1 - ee * magic * magic;
-    double sqrtmagic = Math.sqrt(magic);
+    double sqrtmagic = std::sqrt(magic);
     dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * PI);
-    dlng = (dlng * 180.0) / (a / sqrtmagic * Math.cos(radlat) * PI);
+    dlng = (dlng * 180.0) / (a / sqrtmagic * std::cos(radlat) * PI);
     double mglat = lat + dlat;
     double mglng = lng + dlng;
     return {lng * 2 - mglng, lat * 2 - mglat};
